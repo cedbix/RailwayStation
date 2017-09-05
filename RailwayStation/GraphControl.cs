4c65431f4300d88f575abc9167702f9e1f44b609
Egor <egor@dubna.tk>
2017-09-05 17:16:12 +0300

    GraphControl added

---
+++ RailwayStation/GraphControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace RailwayStation
+{
+    class GraphControl : FrameworkElement
+    {
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            var position = e.GetPosition(this);
+
+            MessageBox.Show($"X={position.X}; Y={position.Y}");
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            drawingContext.DrawRectangle(Brushes.Transparent, new Pen(Brushes.Black, 1), new Rect(0, 0, ActualWidth, ActualHeight));
+            drawingContext.DrawRectangle(Brushes.Aqua, new Pen(Brushes.Black, 1), new Rect(20, 30, 100, 100));
+        }
+
+
+    }
+}
